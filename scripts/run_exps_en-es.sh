748da8893fa1454f7b4f79278c8fdda598c3d201
Daniel Beck <daniel.e.beck@gmail.com>
2016-12-24 20:26:23 +1100

    oops

--- scripts/run_exps_en-es.sh
+++ scripts/run_exps_en-es.sh
@@ -1,7 +1,7 @@
-nohup run_exp.py en-es 1000 30 1 ../results > nohups/1.out
-nohup run_exp.py en-es 1000 30 2 ../results > nohups/2.out
-nohup run_exp.py en-es 1000 30 3 ../results > nohups/3.out
-nohup run_exp.py en-es 1000 30 4 ../results > nohups/4.out
-nohup run_exp.py en-es 1000 30 5 ../results > nohups/5.out
-nohup run_exp.py en-es 1000 30 6 ../results > nohups/6.out
-nohup run_exp.py en-es 1000 30 7 ../results > nohups/7.out
+nohup python run_exp.py en-es 1000 30 1 ../results > nohups/1.out
+nohup python run_exp.py en-es 1000 30 2 ../results > nohups/2.out
+nohup python run_exp.py en-es 1000 30 3 ../results > nohups/3.out
+nohup python run_exp.py en-es 1000 30 4 ../results > nohups/4.out
+nohup python run_exp.py en-es 1000 30 5 ../results > nohups/5.out
+nohup python run_exp.py en-es 1000 30 6 ../results > nohups/6.out
+nohup python run_exp.py en-es 1000 30 7 ../results > nohups/7.out
