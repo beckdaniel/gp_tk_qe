0bd4f61a64337dc4267b86a7cdf0ffbe5dcd8e7f
Daniel Beck <daniel.e.beck@gmail.com>
2017-01-10 14:47:40 +1100

    en-de exps

---
+++ scripts/run_exps_en-de.sh
@@ -0,0 +1,7 @@
+nohup python run_exp.py en-de 3000 30 1 ../results 0 1 > nohups/en-de_1.out
+nohup python run_exp.py en-de 3000 30 2 ../results 0 1 > nohups/en-de_2.out
+nohup python run_exp.py en-de 3000 30 3 ../results 0 1 > nohups/en-de_3.out
+nohup python run_exp.py en-de 3000 30 4 ../results 0 1 > nohups/en-de_4.out
+nohup python run_exp.py en-de 3000 30 5 ../results 0 1 > nohups/en-de_5.out
+nohup python run_exp.py en-de 3000 30 6 ../results 0 1 > nohups/en-de_6.out
+nohup python run_exp.py en-de 3000 30 7 ../results 0 1 > nohups/en-de_7.out
